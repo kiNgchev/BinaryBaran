c8299ef5a8ebc23748de436147d315f11b324177
KiNgchev <kingchev.net@gmail.com>
2025-05-11 17:29:03 +0300

    feat: initial commit

---
+++ src/main/kotlin/net/kingchev/binarybaran/BinaryBaranApplication.kt
@@ -0,0 +1,11 @@
+package net.kingchev.binarybaran
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BinaryBaranApplication
+
+fun main(args: Array<String>) {
+    runApplication<BinaryBaranApplication>(*args)
+}
