aac74f28c1cb5815fb274f5f917478ee46a444a1
KiNgchev <kingchev.net@gmail.com>
2025-08-13 22:21:58 +0300

    feat: init
    Changes:
    - Add `артем` command
    - Add Telegram client

---
+++ baran-worker/src/main/kotlin/net/kingchev/worker/commands/ArtemCommand.kt
@@ -0,0 +1,14 @@
+package net.kingchev.worker.commands
+
+import eu.vendeli.tgbot.TelegramBot
+import eu.vendeli.tgbot.annotations.CommandHandler
+import eu.vendeli.tgbot.annotations.CommonHandler
+import eu.vendeli.tgbot.api.message.sendMessage
+import eu.vendeli.tgbot.types.User
+import eu.vendeli.tgbot.types.component.MessageUpdate
+import eu.vendeli.tgbot.types.component.UpdateType
+
+@CommonHandler.Regex("[Аа]рт[её]м", scope = [UpdateType.MESSAGE])
+public suspend fun artem(update: MessageUpdate, user: User, client: TelegramBot) {
+    sendMessage { "артем" }.send(update.message.chat, client)
+}
