38b18de12ac17074c28a7f50b631864ccb8c4f53
KiNgchev <kingchev.net@gmail.com>
2025-08-14 18:03:14 +0300

    feat: add new commands

--- baran-worker/src/main/kotlin/net/kingchev/worker/commands/RoleplayCommands.kt
+++ baran-worker/src/main/kotlin/net/kingchev/worker/commands/RoleplayCommands.kt
@@ -6,18 +6,21 @@
 package net.kingchev.worker.commands
 
 import eu.vendeli.tgbot.TelegramBot
-import eu.vendeli.tgbot.annotations.CommandHandler
-import eu.vendeli.tgbot.annotations.CommonHandler
+import eu.vendeli.tgbot.annotations.UpdateHandler
 import eu.vendeli.tgbot.api.message.sendMessage
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.component.MessageUpdate
 import eu.vendeli.tgbot.types.component.UpdateType
 
-@CommonHandler.Regex("[О, о]тсосать @([\\w]+)", scope = [UpdateType.MESSAGE])
+@UpdateHandler([UpdateType.MESSAGE])
 public suspend fun blowjob(update: MessageUpdate, user: User, client: TelegramBot) {
-    val matchRes = update.message.text?.let { Regex("[О, о]тсосать @([\\w]+)").find(it) }!!
+    if (!update.text.startsWith("отсосать", true)) return
+    val args = update.text.split(" ")
+    if (args.size < 2)
+        return sendMessage { "Кому отсосать хочешь?" }.send(update.message.chat, client)
 
-    val username = matchRes.groupValues[1]
-    sendMessage { "@${user.username} отсосал @$username" }.send(update.message.chat, client)
+    val target = args[1]
+
+    sendMessage { "@${user.username} отсосал $target" }.send(update.message.chat, client)
 }
 
